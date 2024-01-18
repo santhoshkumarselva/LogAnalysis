a799f69eea619d1ccdb1b04d381101a71e9dc323
Santhosh <santhoshkumarselvaraj205@gmail.com>
2024-01-18 10:16:26 +0530

    Initial commit
    Java program to analyse the issue logs.

---
+++ src/main/java/com/santhosh/loganalysis/RegularExpressionMatcher.java
@@ -0,0 +1,70 @@
+package main.java.com.santhosh.loganalysis;
+
+import utils.FileProcessor;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularExpressionMatcher {
+    public static void matchRegularExpressionsWithLines(String[] lines, String xmlContent) {
+        try {
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            InputSource src = new InputSource();
+            src.setCharacterStream(new StringReader(xmlContent));
+            Document doc = builder.parse(src);
+            NodeList expressions = doc.getElementsByTagName("expression");
+            NodeList notFoundStrings = doc.getElementsByTagName("not-found");
+
+            int previousMatchedLine = -1; // Initialize with -1 to indicate no previous match
+            for (int i = 0; i < expressions.getLength(); i++) {
+                String pattern = expressions.item(i).getTextContent();
+                String notFoundString = notFoundStrings.item(i).getTextContent();
+                Pattern p = Pattern.compile(pattern);
+                boolean matched = false;
+
+                for (int j = previousMatchedLine + 1; j < lines.length; j++) {
+                    String line = lines[j];
+                    Matcher m = p.matcher(line);
+                    if (m.find()) {
+                        matched = true;
+                        System.out.println(line);
+                        previousMatchedLine = j; // Update the previous matched line
+                        break;
+                    }
+                }
+
+                if (!matched) {
+                    System.out.println(notFoundString);
+                    break; // Stop checking further regular expressions
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String[] lines = FileProcessor.readLinesFromFile("wired-connection.log");
+        String xmlString;
+        try {
+            xmlString = FileProcessor.xmlToString("wired_carplay_connection.xml");
+        } catch (SAXException e) {
+            throw new RuntimeException(e);
+        } catch (TransformerException e) {
+            throw new RuntimeException(e);
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+        matchRegularExpressionsWithLines(lines, xmlString);
+    }
+}
---
+++ src/main/utils/FileProcessor.java
@@ -0,0 +1,44 @@
+package utils;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.util.ArrayList;
+
+public class FileProcessor {
+    public static String[] readLinesFromFile(String filePath) throws IOException {
+        ArrayList<String> lines = new ArrayList<>();
+        ClassLoader classLoader = FileProcessor.class.getClassLoader();
+        BufferedReader reader = new BufferedReader(new FileReader(new File(classLoader.getResource(filePath).getFile())));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        reader.close();
+        return lines.toArray(new String[0]);
+    }
+    public static String xmlToString(String filePath) throws IOException, SAXException, TransformerException, ParserConfigurationException {
+        ClassLoader classLoader = FileProcessor.class.getClassLoader();
+        File xmlFile = new File(classLoader.getResource(filePath).getFile());
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(xmlFile);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        StringWriter stringWriter = new StringWriter();
+        transformer.transform(new DOMSource(document), new StreamResult(stringWriter));
+
+        String xmlString = stringWriter.getBuffer().toString();
+        return xmlString;
+    }
+}
